e22cb07f9c7dddbd9750dc8f1031d7c6b4f28477
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2024-12-03 21:48:09 -0500

    Form link update, container sizing

--- src/app/lanzate/page.tsx
+++ src/app/lanzate/page.tsx
@@ -6,16 +6,15 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='w-full mx-auto h-full bg-charcoal pt-40 pb-20'> 
+    <div className='w-full mx-auto h-full bg-charcoal pt-28 pb-0'> 
 
-    <div className='mb-10 text-white w-auto text-center'>
-      <h1 className='text-4xl md:text-5xl text-center px-5 max-w-7xl mx-auto font-light italic '>Stay Connected with Florece Consulting</h1>
-      <p className='max-w-screen-lg mx-auto text-center p-5'>Sign up for our email list to receive exclusive insights, resources, and updates on how we empower communities through strategy and growth. Join us in shaping a brighter future!</p>
-    </div>
+    {/* <div className='mb-10 text-white w-auto text-center'>
+      <h1 className='text-4xl md:text-6xl text-center px-5 max-w-7xl mx-auto font-light italic '>floreceComms Newsletter</h1>
+    </div> */}
         <div className='w-auto '>
         
         <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLSeVT8WHgRoq6TSAZmGy8Lkl_SlOagzhonUrYjuXOrnRwhfsnA/viewform?embedded=true"
+              src="https://docs.google.com/forms/d/e/1FAIpQLSdrGBPGvcNoNV4hkWUTGI28dPQPXM7RrgSuYsq92qZ0GwjYTg/viewform?"
               loading="lazy"
               allowFullScreen={false}
               width="100%"
@@ -23,11 +22,11 @@ function page() {
             ></iframe>
         
         </div>
-        <div className="w-full h-auto my-10 flex justify-center">
+        {/* <div className="w-full h-auto my-10 flex justify-center">
         <Button className="border-2 border-lavender bg-lavender text-charcoal rounded-none hover:rounded-lg hover:border-lavender uppercase tracking-wide hover:drop-shadow-lg hover:text-white hover:bg-charcoal transition-all">
           <Link href="/">Back</Link>
         </Button>
-      </div>
+      </div> */}
         </div>
    
   )
